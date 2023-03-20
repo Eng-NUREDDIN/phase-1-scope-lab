2a024b2fae2768efdacb9e2f80b9a340f9df520c
Eng-NUREDDIN <nour.hammoda@gmail.com>
2023-03-20 04:28:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName="bob";
+var bestCustomer ;
+const leastFavoriteCustomer="nour";
+function upperCaseCustomerName(){
+    customerName= customerName.toLocaleUpperCase();
+    return customerName;
+}
+function setBestCustomer(){
+     bestCustomer ="not bob"   
+}
+function overwriteBestCustomer(name){
+    bestCustomer=name;
+}
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer="bob";
+}
